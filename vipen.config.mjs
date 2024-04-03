b6b2363837d598985d00c107304ad02d56628ad7
eXory2024 <exory@anio.software>
2024-04-04 01:10:01 +0200

    Upgrade to new vipen version.

--- vipen.config.mjs
+++ vipen.config.mjs
@@ -1,4 +1,4 @@
-import {searchAndReplace, copyFile} from "vipen/processing"
+import {generateFromTemplate} from "vipen/autogenerate"
 
 const asyncToSync = {
 	"async function copySymbolicLink(": "function copySymbolicLink(",
@@ -22,8 +22,8 @@ export default {
 	realm: "js",
 	type: "package",
 
-	preprocessing: [
-		searchAndReplace("src/template.mjs", asyncToSync, "src/auto/sync.mjs"),
-		copyFile("src/template.mjs", "src/auto/async.mjs")
-	]
+	autogenerate: {
+		"sync.mjs": generateFromTemplate("src/template.mjs", asyncToSync),
+		"async.mjs": generateFromTemplate("src/template.mjs", {})
+	}
 }
