a3e6e35c0573f6dd18afd5894c23eed033b61c3a
eXory2024 <exory@anio.software>
2025-06-29 00:50:40 +0200

    Add copySymbolicLinkXXX stub function.

---
+++ project/src/__copySymbolicLinkXXX.as.ts
@@ -0,0 +1,16 @@
+import type {EnkoreJSRuntimeContext} from "@anio-software/enkore.js-runtime"
+import type {CopyOptions} from "#~export/CopyOptions.ts"
+import type {PathInformation} from "@anio-software/pkg.node-fs-stat-path"
+
+export async function copySymbolicLink(
+//>export function copySymbolicLinkSync(
+	context: EnkoreJSRuntimeContext,
+	options: CopyOptions,
+	pathInformation: PathInformation,
+	source: string,
+	destination: string
+): Promise<boolean> {
+//>): boolean {
+
+	return false
+}
