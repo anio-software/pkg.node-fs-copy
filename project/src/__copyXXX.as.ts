b16e405a2f7f297a98f129d37fd17d2314d73aa7
eXory2024 <exory@anio.software>
2025-06-29 00:59:50 +0200

    Partially implement copy function.

--- project/src/__copyXXX.as.ts
+++ project/src/__copyXXX.as.ts
@@ -9,12 +9,40 @@ export type {__EnkoreFunctionDependencies}
 
 import type {CopyOptions} from "#~export/CopyOptions.ts"
 
+import {copyAnything} from "#~src/copyAnything.ts"
+//>import {copyAnythingSync as copyAnything} from "#~src/copyAnythingSync.ts"
+
 export async function __implementation(
 //>export function __implementationSync(
 	contextOptions: EnkoreJSRuntimeContextOptions,
 	dependencies: __EnkoreFunctionDependencies,
 	options: CopyOptions
-): Promise<undefined> {
-//>): undefined {
+): Promise<boolean> {
+//>): boolean {
 	const context = createContext(contextOptions, 0)
+
+	const pathType = await dependencies.getTypeOfPath(options.source)
+//>	const pathType = dependencies.getTypeOfPath(options.source)
+
+	const pathInfo = await dependencies.getPathInformation(options.source)
+//>	const pathInfo = dependencies.getPathInformation(options.source)
+
+	if (
+	    pathType === "error"       ||
+	    pathType === "nonExisting"
+	    ) {
+		context.log.warn(`unable to copy path '${options.source}' due to an error.`)
+
+		return false
+	}
+
+	return await copyAnything(
+//>	return copyAnything(
+		context,
+		options,
+		pathType,
+		pathInfo,
+		options.source,
+		options.destination
+	)
 }
