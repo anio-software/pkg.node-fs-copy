94acdb5b7ab0b6f632f3611efc4f21e6921e8388
eXory2024 <exory@anio.software>
2025-06-29 14:35:48 +0200

    Initial implementation of copyDirectory.

--- project/src/__copyDirectoryXXX.as.ts
+++ project/src/__copyDirectoryXXX.as.ts
@@ -5,6 +5,14 @@ import type {PathInformation} from "@anio-software/pkg.node-fs-stat-path"
 import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/Dependencies.ts"
 //>import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/DependenciesSync.ts"
 
+import {mkdir} from "@anio-software/pkg-private.node-consistent-fs/async"
+//>import {mkdir} from "@anio-software/pkg-private.node-consistent-fs/sync"
+import {copyAnything} from "#~src/copyAnything.ts"
+//>import {copyAnythingSync as copyAnything} from "#~src/copyAnythingSync.ts"
+
+import {getOrCreateError} from "@anio-software/pkg.js-utils"
+import path from "node:path"
+
 export async function copyDirectory(
 //>export function copyDirectorySync(
 	context: EnkoreJSRuntimeContext,
@@ -17,5 +25,41 @@ export async function copyDirectory(
 //>): boolean {
 	context.log.trace(`copy directory '${source}'`)
 
-	return false
+	try {
+		await mkdir(destination, {
+//>		mkdir(destination, {
+			mode: pathInformation.permissions.mode,
+			recursive: false
+		})
+
+		return await dependencies.scandirCallback(source, {
+//>		return dependencies.scandirCallback(source, {
+			includePathInformation: true,
+			async callback(entry, {stopLoop}) {
+//>			callback(entry, {stopLoop}) {
+				const res = await copyAnything(
+//>				const res = copyAnything(
+					context,
+					dependencies,
+					options,
+					entry.pathType as any,
+					entry.information!,
+					entry.absolutePath,
+					path.join(destination, entry.relativePath)
+				)
+
+				if (!res) {
+					return stopLoop(false)
+				}
+
+				return undefined
+			}
+		})
+	} catch (e) {
+		const error = getOrCreateError(e)
+
+		context.log.warn(`caught exception '${error.message}' while copying directory '${source}'`)
+
+		return false
+	}
 }
