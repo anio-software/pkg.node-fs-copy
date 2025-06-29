176d871669254e86a383c673083acd4784d6abcc
eXory2024 <exory@anio.software>
2025-06-29 15:01:04 +0200

    Initial implementation of copySymbolicLink.

--- project/src/__copySymbolicLinkXXX.as.ts
+++ project/src/__copySymbolicLinkXXX.as.ts
@@ -4,6 +4,10 @@ import type {PathInformation} from "@anio-software/pkg.node-fs-stat-path"
 
 import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/Dependencies.ts"
 //>import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/DependenciesSync.ts"
+import {readlink, symlink} from "@anio-software/pkg-private.node-consistent-fs/async"
+//>import {readlink, symlink} from "@anio-software/pkg-private.node-consistent-fs/sync"
+
+import {getOrCreateError} from "@anio-software/pkg.js-utils"
 
 export async function copySymbolicLink(
 //>export function copySymbolicLinkSync(
@@ -17,5 +21,19 @@ export async function copySymbolicLink(
 //>): boolean {
 	context.log.trace(`copy symbolic link '${source}'`)
 
-	return false
+	try {
+		const link = await readlink(source)
+//>		const link = readlink(source)
+
+		await symlink(link, destination)
+//>		symlink(link, destination)
+
+		return true
+	} catch (e) {
+		const error = getOrCreateError(e)
+
+		context.log.warn(`caught exception '${error.message}' while copying symbolic link '${source}'`)
+
+		return false
+	}
 }
