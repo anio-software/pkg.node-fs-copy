8369043b594124c362906e02886c15670e9baa46
eXory2024 <exory@anio.software>
2025-06-29 13:15:00 +0200

    Initial implementation of copyFile.

--- project/src/__copyFileXXX.as.ts
+++ project/src/__copyFileXXX.as.ts
@@ -5,6 +5,11 @@ import type {PathInformation} from "@anio-software/pkg.node-fs-stat-path"
 import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/Dependencies.ts"
 //>import type {__EnkoreFunctionDependencies as Dependencies} from "#~src/DependenciesSync.ts"
 
+import {copyFile as _copyFile, chmod} from "@anio-software/pkg-private.node-consistent-fs/async"
+//>import {copyFile as _copyFile, chmod} from "@anio-software/pkg-private.node-consistent-fs/sync"
+
+import {getOrCreateError} from "@anio-software/pkg.js-utils"
+
 export async function copyFile(
 //>export function copyFileSync(
 	context: EnkoreJSRuntimeContext,
@@ -17,5 +22,19 @@ export async function copyFile(
 //>): boolean {
 	context.log.trace(`copy file '${source}'`)
 
-	return false
+	try {
+		await _copyFile(source, destination)
+//>		_copyFile(source, destination)
+
+		await chmod(destination, pathInformation.permissions.mode)
+//>		chmod(destination, pathInformation.permissions.mode)
+
+		return true
+	} catch (e) {
+		const error = getOrCreateError(e)
+
+		context.log.warn(`caught exception '${error.message}' while copying file '${source}'`)
+
+		return false
+	}
 }
